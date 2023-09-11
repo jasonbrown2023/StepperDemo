6b4c86f12f30b03dc9ef40de1963624542ffb9e2
Jason Brown <jasonba@smu.edu>
2023-09-10 20:29:37 -0500

    new main

--- StepperDemo/ViewController.swift
+++ StepperDemo/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func stepper(_ sender: UIStepper) {
+        label.text = String(sender.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
 }
 
